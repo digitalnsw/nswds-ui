0ab1513cbf950f0ad34bd8f581b8cf61a23085cf
laurenhitchon <lauren@hitchon.me>
2026-05-23 16:03:40 +1000

    fix(prettier-config): change singleQuote option from false to true for consistent code style feat(prettier-config): add prettier-plugin-organize-imports to manage imports automatically

--- packages/prettier-config/index.cjs
+++ packages/prettier-config/index.cjs
@@ -2,11 +2,11 @@
 module.exports = {
   endOfLine: "lf",
   semi: false,
-  singleQuote: false,
+  singleQuote: true,
   tabWidth: 2,
   trailingComma: "es5",
   printWidth: 80,
-  plugins: ["prettier-plugin-tailwindcss"],
+  plugins: ["prettier-plugin-organize-imports", "prettier-plugin-tailwindcss"],
   tailwindStylesheet: "packages/ui/src/styles/globals.css",
   tailwindFunctions: ["cn", "cva"],
 }
